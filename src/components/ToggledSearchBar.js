4cdb2e3ab42024b2023b03f275be99c33d90134f
josephine geoghegan <josephineanngeoghegan@gmail.com>
2022-05-30 12:43:05 +0200

    spacing

--- src/components/ToggledSearchBar.js
+++ src/components/ToggledSearchBar.js
@@ -3,8 +3,13 @@ import React from 'react';
 function ToggledSearchBar(props) {
     return (
         <>
-            <div>
-                <div></div>
+            <div id="a" className='flex justify-center items-center'>
+                <div id="e" className='flex justify-between'>
+                </div>
+            </div>
+            <div id="b" className='flex flex-col justify-center sm:flex-row sm:justify-around sm:items-center'>
+                <div id="c" className='flex justify-between items-center'></div>
+                <div id="d" className='flex flex-col justify-between items-center'></div>
             </div>
         </>
     );
