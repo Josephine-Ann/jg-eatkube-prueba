31a59cb6f806e6216846144496392710e92620e0
josephine geoghegan <josephineanngeoghegan@gmail.com>
2022-05-30 11:57:26 +0200

    component created and tailwind added to project

---
+++ src/components/ToggledSearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ToggledSearchBar(props) {
+    return (
+        <>
+            <div>
+                <div></div>
+            </div>
+        </>
+    );
+}
+
+export default ToggledSearchBar;
